70f430af286e44c38136a658fb1d4a178e557a6a
PhilFuster <pafus23@protonmail.com>
2020-04-23 00:25:59 -0400

    Project/Repository setup

--- app.js
+++ app.js
@@ -3,9 +3,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const debug = require('debug');
 
+/*
+  === Local Variables ===
+*/
+// Express Session Ooption Object
+const sess = {
+  secret: 'Phil is cool... duh',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {},
+};
+// Binding debug's output to the console.
+// Initializing Loggers
+debug.log = console.log.bind(console);
+const log = debug('geocoding:app');
+
+// Router declaration
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+/*
+  === Function Definitions ===
+ */
+function errorHandler(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+}
 
 var app = express();
 
@@ -17,25 +46,19 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/routes')));
+app.use(session(sess));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler);
 
 module.exports = app;
